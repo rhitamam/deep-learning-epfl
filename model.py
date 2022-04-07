3b1bc9f3e7e69f6ea5534d9c4a71cdde22f33611
GILLYBOEUF <56752225+GILLYBOEUF@users.noreply.github.com>
2022-04-07 13:34:02 +0200

    Add files via upload

---
+++ model.py
@@ -0,0 +1,18 @@
+### For mini - project 1
+class Model () :
+    def ˙˙init˙˙ ( self ):
+        ## instantiate model + optimizer + loss function + any other stuff you need
+        pass
+
+    def load˙pretrained˙model ( self ):
+        ## This loads the parameters saved in bestmodel .pth into the model
+        pass
+
+    def train ( self , train˙input , train˙target ):
+        #: train˙input : tensor of size (N, C, H, W) containing a noisy version of the images
+        #: train˙target : tensor of size (N, C, H, W) containing another noisy version of the same images , which only differs from the input by their noise .
+        pass
+
+    def predict ( self , test˙input ):
+        #: test˙input : tensor of size (N1 , C, H, W) that has to be denoised by the trained or the loaded network .
+        pass
