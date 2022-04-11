df2bffc9e60520424a3c1340e49873d0ab65cbb8
Marie-Alix GILLYBOEUF <marie-alix@tsf-444-wpa-2-117.epfl.ch>
2022-04-11 11:11:22 +0200

    others file

--- others/otherfile1.py
+++ others/otherfile1.py
@@ -1,17 +1,5 @@
 import torch
 from torch.utils.data import random_split
-import torchvision.transforms as T
-
-def data_transform(imgs):
-    transform = T.Compose([
-                    T.RandomResizedCrop(224),
-                    T.RandomHorizontalFlip(),
-                    T.RandomVerticalFlip(p=0.5),
-                    T.ToTensor(),
-                    T.Normalize(mean=[0.485, 0.456, 0.406],
-                            std=[0.229, 0.224, 0.225])
-    ])
-    return transform(imgs)
 
 def split_train_test (imgs, split_ratio = 0.8):
     return train, test = random_split(imgs, 
