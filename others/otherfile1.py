ae7afad94f7603fe2901d0983840cd3f3a336610
Marie-Alix GILLYBOEUF <marie-alix@tsf-444-wpa-4-134.epfl.ch>
2022-04-13 09:43:22 +0200

    otherfile1

--- others/otherfile1.py
+++ others/otherfile1.py
@@ -2,5 +2,6 @@ import torch
 from torch.utils.data import random_split
 
 def split_train_test (imgs, split_ratio = 0.8):
-    return train, test = random_split(imgs, 
-                                        [split_ratio * imgs.shape[0], (1 - split_ratio) * imgs.shape[0])
+    #train, test = torch.utils.data.random_split(imgs, [40000, 10000])
+    train, test = torch.utils.data.random_split(imgs, [int(split_ratio * imgs.shape[0]), imgs.shape[0] - int(split_ratio * imgs.shape[0])])
+    return train.dataset, test.dataset
