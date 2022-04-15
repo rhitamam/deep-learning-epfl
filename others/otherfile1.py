4fa16b75b45d7c24a2818884f3161f8231f7a911
Marie-Alix GILLYBOEUF <marie-alix@MacBook-Pro-de-Marie-Alix.local>
2022-04-15 09:48:00 +0200

    suppression of function split

--- others/otherfile1.py
+++ others/otherfile1.py
@@ -1,7 +1,1 @@
 import torch
-from torch.utils.data import random_split
-
-def split_train_test (imgs, split_ratio = 0.8):
-    #train, test = torch.utils.data.random_split(imgs, [40000, 10000])
-    train, test = torch.utils.data.random_split(imgs, [int(split_ratio * imgs.shape[0]), imgs.shape[0] - int(split_ratio * imgs.shape[0])])
-    return train.dataset, test.dataset
