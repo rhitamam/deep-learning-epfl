7cda5f85bf53d93122b655f7caf5f60c6ffaac8d
rhitamam <rhita.mamou@gmail.com>
2022-05-04 13:08:46 +0200

    weight_init

--- Proj_2/utility.py
+++ Proj_2/utility.py
@@ -1,6 +1,8 @@
 #import torch
 from torch import empty
-import math
+
+import random, math
+
 
 def ReLU(x):
     return max(0.0, x)
@@ -10,5 +12,7 @@ def df_ReLU(x):
     x[x>0] = 1
     return x
 
-
+def weight_init(k, kernal_size):
+    t = -2*math.sqrt(k) * torch.rand(kernal_size[0], kernal_size[1]) + math.sqrt(k)
+    
 
