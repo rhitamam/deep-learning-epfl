313662219db64668283f85201761eabc7e0a8141
Assia Ouanaya <assiaouanaya@tsf-436-wpa-1-135.epfl.ch>
2022-05-11 13:06:25 +0200

    oh yea

--- Miniproject_2/model.py
+++ Miniproject_2/model.py
@@ -1,6 +1,6 @@
 from torch import empty , cat , arange, Tensor, manual_seed
 from torch.nn.functional import fold, unfold
-from utility import *
+from others.otherfile1 import *
 import random, math
 random.seed(0)
 manual_seed(0)
@@ -15,7 +15,7 @@ class Module(object) :
     def param (self) :
         return []
 
-class Conv2d(Module) :
+class Conv2d(Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1, padding=0, dilation=1):
         self.in_chan = in_channels
         self.out_chan = out_channels
@@ -37,13 +37,26 @@ class Conv2d(Module) :
         unfolded = unfold(input, kernel_size=self.kernel_size)
         wxb = self.weights.view(self.out_chan, -1) @ unfolded + self.bias.view(1, -1, 1)
         output = wxb.view(input.shape[0], self.out_chan, input.shape[2] - self.kernel_size[0] + 1, input.shape[3] - self.kernel_size[1]+ 1)
+        '''
+        add 0 zeros to the input
+        add stride 
+        unfolded = unfold(input, kernel_size=self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
+        output = wxb.view(input.shape[0], 
+                            self.out_chan,
+                            (1+(input.shape[2] + 2 * self.padding[0] - self.dilation[0] * (self.kernel_size[0] - 1) - 1) / self.stride[0]).floor_(),
+                            (1+(input.shape[3] + 2 * self.padding[1] - self.dilation[1] * (self.kernel_size[1] - 1) - 1) / self.stride[1]).floor_())
+        '''
         return output
         
     def backward (self,  gradwrtoutput):
-        self.grads = gradwrtoutput
+        'test wrt to grad of conv2d !!'
+        self.b_grad = gradwrtoutput
+        
+        fold = fold(output_size=self.w_grad.shape, kernel_size=self.kernel_size)
+        self.w_grad = gradwrtoutput @ fold(self.input)
+        return (self.w_grad, self.b_grad)
 
-
-    def param (self) :
+    def param (self):
         """
         Return a list of pairs composed of a parameter tensor and a gradient tensor of the same size
         Output: 
@@ -51,23 +64,20 @@ class Conv2d(Module) :
         """
         return [(self.weights, self.w_grad), (self.bias, self.b_grad)]
 
-class SGD (object) :
-    def __init__(self, lr=0.1, momentum=0.9):
-        self.tensor = Tensor()
+class SGD(object) :
+    def __init__(self, module, lr=0.1, momentum=0.9):
+        self.module = module
         self.lr = lr
         self.momentum = momentum
 
-    def forward (self,  input) :
-        self.input = input 
-        return 
+    def update (self):
+        weights, w_grad, bias, b_grad = self.module.params()
+        weights -= self.lr * w_grad
+        bias -= self.lr * b_grad
+        return (weights, bias)
 
-    def backward (self,  gradwrtoutput):
-        raise NotImplementedError
 
-    def param (self) :
-        return []
-
-class Sequential (object) :
+class Sequential(Module) :
     def __init__(self, modules):
         self.modules = modules
 
@@ -79,12 +89,15 @@ class Sequential (object) :
     def backward (self, gradwrtoutput):
         self.gradwrtoutput = gradwrtoutput
         for module in self.modules:
-            gradwrtoutput = module.forward(gradwrtoutput)
+            gradwrtoutput = module.backward(gradwrtoutput)
         return gradwrtoutput
+    
     def param (self) :
+        'should return multiple params'
         return []
 
-class MSE (object) :
+
+class MSE(Module) :
     def __init__(self):
         self.tensor = Tensor()
         self.target = Tensor()
@@ -92,20 +105,20 @@ class MSE (object) :
     def forward (self,  input, target):
         self.tensor = input
         self.target = target
-        return MSE(input, target)
+        return MSE_func(input, target)
     def backward (self, gradwrtoutput):
         return dMSE(input, self.target) 
     def param (self) :
         return []
 
 
-class ReLU (object) :
+class ReLU(Module) :
     def __init__(self):
         self.tensor = Tensor()
 
     def forward (self,  input):
         self.tensor = input
-        return ReLU(input)
+        return ReLU_func(input)
 
     def backward (self, gradwrtoutput):
         return gradwrtoutput * dReLU(self.tensor)
@@ -114,13 +127,13 @@ class ReLU (object) :
         return []
 
 
-class Sigmoid (object) :
+class Sigmoid(Module) :
     def __init__(self):
         self.tensor = Tensor()
 
     def forward (self,  input):
         self.tensor = input
-        return Sigmoid(input)
+        return Sigmoid_func(input)
 
     def backward (self, gradwrtoutput):
         return gradwrtoutput * dSigmoid(self.tensor)
@@ -147,5 +160,5 @@ report:
 - plot 
 - partie théorique
 - réutilisation de l'intro ?
-- 
+- rapport 2 individuels marquer la séparation ?
 '''
