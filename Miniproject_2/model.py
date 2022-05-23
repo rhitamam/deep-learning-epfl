2c4abf886b0d918eb28da825c50bc90cbf1d4da6
Marie-Alix GILLYBOEUF <marie-alix@tsf-444-wpa-5-102.epfl.ch>
2022-05-23 16:10:40 +0200

    last version of backward conv2d

--- Miniproject_2/model.py
+++ Miniproject_2/model.py
@@ -1,3 +1,4 @@
+from black import out
 from torch import empty , cat , arange, Tensor, manual_seed, repeat_interleave
 from torch.nn.functional import fold, unfold
 #from Miniproject_2.others.otherfile1 import * #To uncomment for the final submission
@@ -29,53 +30,73 @@ class Conv2d(Module):
         self.input = Tensor()
     
     def forward (self,  input) :
-        #add padd stride and everything
-        print("f conv2")
-        print("in")
         self.input = input
-       
-        unfolded = unfold(input, kernel_size=self.kernel_size)
-        print('yoho', self.weights.view(self.out_chan, -1).shape, unfolded.shape )
-        wxb = self.weights.view(self.out_chan, -1) @ unfolded 
-        if self.bias_bool:
-            wxb += self.bias.view(1, -1, 1)
-        output = wxb.view(input.shape[0], 
-                        self.out_chan, 
-                        input.shape[2] - self.kernel_size[0] + 1, 
-                        input.shape[3] - self.kernel_size[1]+ 1)
-        '''
         unfolded = unfold(input, kernel_size=self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
-        print('unfolded',input.shape,unfolded.shape)
-        print('weights', self.weights.shape, self.weights.view(self.out_chan, -1).shape)
-        print('biases', self.bias.view(1, -1, 1).shape)
         wxb = self.weights.view(self.out_chan, -1) @ unfolded + self.bias.view(1, -1, 1)
         output = wxb.view(input.shape[0], 
                     self.out_chan,
                     math.floor(1+(input.shape[2] + 2 * self.padding[0] - self.dilation[0] * (self.kernel_size[0] - 1) - 1) / self.stride[0]),
                     math.floor(1+(input.shape[3] + 2 * self.padding[1] - self.dilation[1] * (self.kernel_size[1] - 1) - 1) / self.stride[0]))
-         '''
-        
-        print("out", output.shape)
         return output
         
-    def backward(self, gradwrtoutput):
-        print("b conv2")
-        print("in", gradwrtoutput.shape, self.weights.shape, self.input.shape)
-        unfolded = unfold(self.input, kernel_size=self.kernel_size)
-        print(gradwrtoutput.view(self.out_chan, -1).shape,unfolded.shape)
-        self.w_grad = (gradwrtoutput.view(self.out_chan, -1) @ unfolded.squeeze(0).transpose(1,2)).view(self.w_grad.size())
-        '''
-        self.b_grad = gradwrtoutput.mean((0,2,3))
-        unfolded = unfold(self.input, kernel_size=self.kernel_size, dilation=self.dilation, padding=self.padding, stride=self.stride)
-        self.w_grad =  gradwrtoutput @ unfolded.view((self.input.shape[0], self.input.shape[1], gradwrtoutput.shape[2], -1))
+    def backward_bis(self, gradwrtoutput):
         ''' 
+        unfolded = unfold(self.input, kernel_size=self.kernel_size)
+        self.w_grad = (gradwrtoutput.view(self.out_chan, -1) @ unfolded).view(self.w_grad.size())
+        '''
+        unfolded = unfold(self.input, 
+                            kernel_size=self.kernel_size, 
+                            dilation=self.dilation, 
+                            padding=self.padding, 
+                            stride=self.stride)
+        print ("unfolded", unfolded.view((-1, self.input.shape[1], gradwrtoutput.shape[2], gradwrtoutput.shape[3])).squeeze().shape)
+        self.w_grad =  gradwrtoutput @ unfolded.view((self.input.shape[0], self.input.shape[1], gradwrtoutput.shape[2], -1))
+        
         if self.bias_bool:
-            self.b_grad = gradwrtoutput.mean((0,2,3))
-        dl_dx_old = self.weights.view(self.out_chan, -1).t() @ gradwrtoutput.view(1, self.out_chan, -1)
-        #dl_dx_old = dl_dx_old.view(self.input.shape)
-        dl_dx_old = fold(dl_dx_old, output_size=self.input.shape[2:], kernel_size=self.kernel_size)
-        print('out', dl_dx_old.shape)
-        return dl_dx_old 
+            self.b_grad = gradwrtoutput.sum((0,2,3))
+
+        dl_dx_old = empty(self.input.shape)
+        for idx, dl_ds in enumerate(gradwrtoutput):  # torch autograd jacobian cf slides torch autograd.grad
+            dl_dx_prev = self.weights.view(self.out_chan, -1).t() @ dl_ds.view(1, self.out_chan, -1)
+            # CHECK FIRST !!! FOLD UNFOLD CONSISTENCY
+            dl_dx_prev_folded = fold(dl_dx_prev, output_size=self.input.size()[2:], kernel_size=self.kernel_size, stride=self.stride)
+            dl_dx_old[idx] = dl_dx_prev_folded
+        #dl_dx_old = dl_dx_old.view((self.input.shape[0], self.input.shape[1], self.input.shape[2],-1))
+        '''
+        dl_dx_old = fold(dl_dx_old, 
+                        output_size=self.input.shape[2:], 
+                        kernel_size=self.kernel_size, 
+                        dilation=self.dilation, 
+                        padding=self.padding, 
+                        stride=self.stride)
+                  '''  
+        print('w g', self.w_grad.shape)
+        print('b g', self.b_grad.shape)    
+        return dl_dx_old
+    
+    def backward(self, gradwrtoutput):
+        dl_dx_old = []
+        print("b conv")
+        for dl_ds in gradwrtoutput:
+            dl_dx_old_j = self.weights.view(self.out_chan, -1).t() @ dl_ds.view(1, self.out_chan, -1) 
+            dl_dx_old_j = fold(dl_dx_old_j, output_size=self.input.shape[2:], kernel_size=self.kernel_size)
+            dl_dx_old.append(dl_dx_old_j)
+            
+            for x_old in self.input:
+                unfolded = unfold(x_old.unsqueeze(0), kernel_size=self.kernel_size)
+                #print("view", (dl_ds.view(self.out_chan, -1).shape))
+                if (torch.isnan(dl_ds.reshape(self.out_chan, unfolded.shape[2]))).any():
+                    print("nan")
+                    break
+                self.w_grad.add_((dl_ds.reshape(self.out_chan, unfolded.shape[2]) @ unfolded.squeeze(0).t()).view(self.w_grad.size()))
+        
+        if self.bias_bool:
+            self.b_grad = gradwrtoutput.sum((0,2,3))
+        
+        dl_dx_old = torch.cat(dl_dx_old)
+        print('w g', self.w_grad.shape)
+        print('b g', self.b_grad.shape)
+        return (dl_dx_old)
 
     def param(self):
         """
@@ -86,7 +107,7 @@ class Conv2d(Module):
         if self.bias_bool:
             return [(self.weights, self.w_grad), (self.bias, self.b_grad)]
         else:
-            (self.weights, self.w_grad)
+            [(self.weights, self.w_grad)]
 
     def zero_grad(self):
         'set all the gradient of the weight and the bias to zero'
@@ -105,26 +126,16 @@ class NearestUpSampling(Module) :
         self.scalefactor = scalefactor
     
     def forward(self, input):
-        print("f near")
-        print("in")
-        print(input.shape)
         first = repeat_interleave(input, 2, dim=3)
-        final =  repeat_interleave(first,2,dim=2)
-        print("out")
-        print(final.shape)
-        
+        final =  repeat_interleave(first,2,dim=2)        
         return final
 
     def backward(self, gradwrtoutput):
-        print("b near")
-        print("in")
-        print(gradwrtoutput[0].shape)
         if isinstance(gradwrtoutput,tuple):
             weights, bias = gradwrtoutput
         else:
             weights = gradwrtoutput
         u = unfold(weights,kernel_size=self.scalefactor, stride=2)
-        #print((weights.size()[2]/self.scalefactor)*(weights.size()[2]/self.scalefactor))
         viewed = u.view(weights.shape[0],
                         weights.size()[1],
                         self.scalefactor*self.scalefactor,
@@ -135,8 +146,6 @@ class NearestUpSampling(Module) :
                             weights.size()[1],
                             int(weights.size()[2]/2),
                             int(weights.size()[2]/2))
-        print("out b")
-        print(final.shape)
         return final
 
 
@@ -151,11 +160,9 @@ class Sequential(Module) :
             out = module.forward(out)
 
         self.input = out
-        #print(out.size())
         return out
     
     def backward (self, gradwrtoutput):
-        print(gradwrtoutput.shape)
         for module in reversed(self.modules):
             gradwrtoutput = module.backward(gradwrtoutput)
         return gradwrtoutput
@@ -209,11 +216,12 @@ class Model(Module):
             for b in range(0, train_input.size(0), self.mini_batch_size):
                 input = train_input[b: b + self.mini_batch_size]
                 target = train_target[b: b+ self.mini_batch_size]
-                print('hoyo', input.shape)
                 #forward pass for the model sequential
                 output = self.model.forward(input)
+
                 #loss at the end of the forward pass
                 loss = self.criterion.forward(output,target)
+                #print('loss', loss)
                 acc_loss += loss
 
                 #set the gradients to zero before starting to do backpropragation
@@ -233,7 +241,7 @@ class Model(Module):
             
 
     def predict(self, test_input):
-        return self.model(test_input)
+        return self.model.forward(test_input)
 
     def load_pretrained_model(self):
         import pickle
@@ -267,7 +275,7 @@ class Model(Module):
         else:
             raise RuntimeError('Error: FILE must be a string')
     
-    def psnr(denoised , ground_truth):
+    def psnr(self, denoised , ground_truth):
         # Peak Signal to Noise Ratio: denoised and ground_truth have range [0, 1] 
         denoised = denoised.float()
         ground_truth = ground_truth.float()/255
@@ -299,12 +307,9 @@ class ReLU(Module) :
 
     def forward (self,  input):
         self.tensor = input
-        print('relu', input.shape)
         return ReLU_func(input)
 
     def backward (self, gradwrtoutput):
-        print("b relu")
-        print('out', (gradwrtoutput * dReLU(self.tensor)).shape)
         return gradwrtoutput * dReLU(self.tensor)
 
     def param (self) :
@@ -320,8 +325,6 @@ class Sigmoid(Module):
         return Sigmoid_func(input)
 
     def backward (self, gradwrtoutput):
-        print("b sigmoid")
-        print('out', (gradwrtoutput * dSigmoid(self.tensor)).shape)
         return gradwrtoutput * dSigmoid(self.tensor)
 
     def param (self) :
@@ -335,27 +338,31 @@ class SGD(object) :
         self.momentum = momentum
 
     def step (self):
-        weight, w_grad, bias, b_grad = self.module.params()
-        weight -= self.lr * w_grad
-        bias -= self.lr * b_grad
-        self.module.set_weight_and_bias(weight, bias)
+        param = self.module.param()
+            
+        for p, m in zip(param, self.module.modules ):
+            if isinstance(m, Conv2d):
+                #update weight
+                #print("old w", m.weights)
+                #print("p0", p[0])
+                #print("p00", p[0][0])
+                #print("p01", p[0][1])
+                weights = p[0][0] - (self.lr * p[0][1])
+                #print("new w", weights)
+                #update bias
+                #print("old b", m.bias)
+                #print("p1", p[1])
+                #print("p10", p[1][0])
+                #print("p11", p[1][1])
+                bias = p[1][0] - (self.lr * p[1][1])
+                #print("new b", bias)
+                m.set_weights_and_bias(weights, bias)
+                #print("set param", m.param())
     
 
     def zero_grad(self) :
         'set all the gradient of the weight and the bias to zero'
         self.module.zero_grad()
-
-'''
-Conv2d, TransposeConv2d or NearestUpsampling, ReLU, Sigmoid, MSE, SGD, Sequential.
-• Convolution layer.
-• Transpose convolution layer, or alternatively a combination of Nearest neighbor upsampling + Convolution.
-• Upsampling layer, which is usually implemented with transposed convolution, but you can alternatively use a combination of Nearest neighbor upsampling + Convolution for this mini-project.
-• ReLU
-• Sigmoid
-• A container like torch.nn.Sequential to put together an arbitrary configuration of modules together.
-• Mean Squared Error as a Loss Function
-• Stochastic Gradient Descent (SGD) optimizer
-'''
 
 #==============================================================================
 
@@ -364,50 +371,16 @@ import torch
 from torch.nn import functional
 random.seed(0)
 torch.manual_seed(0)
-'''
-in_channels = 4 
-out_channels = 4
-kernel_size = 3
-x = torch.randn((1, in_channels , 32, 32))
-xtarg = torch.randn((1, in_channels , 32, 32))
-y = torch.randn((1, in_channels , 32, 32))
 
-import torch.nn as nn
-
-criterion = MSE()
-
-seq= Sequential(Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=10), 
-                ReLU(),
-                Conv2d(in_channels= out_channels, out_channels= out_channels, kernel_size=10), 
-                ReLU(), 
-                NearestUpSampling(2), 
-                Conv2d(out_channels, out_channels, kernel_size=5), 
-                Conv2d(out_channels, out_channels, kernel_size=5), 
-                ReLU(),
-                NearestUpSampling(2), 
-                Conv2d(out_channels, in_channels, kernel_size=5),
-                Conv2d(out_channels, out_channels, kernel_size=5), 
-                Sigmoid())
-
-optimizer= SGD(seq)
-
-output = seq.forward(x)
-loss = criterion.forward(output,y)
-optimizer.zero_grad()
-gradwrtout = criterion.backward()
-print('dldy', gradwrtout.shape, y.shape)
-seq.backward(gradwrtout)
-'''
-
-noisy_imgs_1 , noisy_imgs_2 = torch.load('data/train_data.pkl')
+noisy_imgs_1 , noisy_imgs_2 = torch.load('../data/train_data.pkl')
 noisy_imgs_1 = noisy_imgs_1[:50] / 255.0
 noisy_imgs_2 = noisy_imgs_2[:50]/ 255.0
-noisy_imgs, clean_imgs = torch.load('data/val_data.pkl')
+noisy_imgs, clean_imgs = torch.load('../data/val_data.pkl')
 noisy_imgs = noisy_imgs[:50] / 255.0
 clean_imgs = clean_imgs[:50] / 255.0
 
 model = Model()
-model.train(noisy_imgs_1, noisy_imgs_2, 100, save_model=True)
+model.train(noisy_imgs_1, noisy_imgs_2, 2, save_model=True)
 prediction = model.predict(noisy_imgs)
 nb_test_errors = model.psnr(prediction, clean_imgs)
 print('test error Net', nb_test_errors)
