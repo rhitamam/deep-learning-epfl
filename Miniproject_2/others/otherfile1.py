fdb1d614bfeaa16c946b159bc0537ba8222867c1
Marie-Alix GILLYBOEUF <marie-alix@MacBook-Pro-de-Marie-Alix.local>
2022-05-24 10:47:55 +0200

    dMSE

--- Miniproject_2/others/otherfile1.py
+++ Miniproject_2/others/otherfile1.py
@@ -8,7 +8,7 @@ def MSE_func(v, t):
     return (v - t).pow(2).mean()
 
 def dMSE(v, t):
-    return 2 * (v - t)
+    return 2 * (v - t).mean()
 
 def ReLU_func(x):
     x_ = x.clone() # make an exact copy to allow safe manipulations of the image
